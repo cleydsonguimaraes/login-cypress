1b9a75d737cf3f1b4fd673bc4193f7f310ebabdb
Cleydson Guimaraes <cleydsonguimaraes048@gmail.com>
2025-03-26 22:14:16 -0300

    chance library added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,6 +5,7 @@ import DashboardPage from "../pages/dashboardPage.js";
 import MenuPage from "../pages/menuPage.js";
 import MyInfoPage from "../pages/myInfoPage.js";
 
+const chance = require("chance").Chance();
 const loginPage = new LoginPage();
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
@@ -19,21 +20,13 @@ describe("Orange HRM Tests", () => {
     );
     dashboardPage.accessDashboardPage();
     menuPage.accessMyInfoPage();
-    myInfoPage.employeeName("Cleydson", "Guimaraes", "Silva");
-    myInfoPage.employeeData("4002", "8922", "110710", "2025-05-17");
-    myInfoPage.otherEmployeeData("Brazilian", "Married", "2026-10-17", "Male");
-    myInfoPage.savePersonalDetails();
-  });
-  it("User Info Update(Green) - Sucess", () => {
-    loginPage.accessLoginPage();
-    loginPage.loginWithUser(
-      userData.userSuccess.username,
-      userData.userSuccess.password
-    );
-    dashboardPage.accessDashboardPage();
-    menuPage.accessMyInfoPage();
-    myInfoPage.employeeName("Cleydson", "Guimaraes", "Silva");
-    myInfoPage.employeeData("4002", "8922", "110710", "2025-05-17");
+    myInfoPage.employeeName(chance.first(), chance.last(), chance.last());
+    myInfoPage.employeeData(
+      chance.natural({ min: 4, max: 4 }),
+      chance.prime({ min: 4, max: 6 }),
+      chance.prime({ min: 4, max: 6 }),
+      "2026-10-17"
+      );
     myInfoPage.otherEmployeeData("Brazilian", "Married", "2026-10-17", "Male");
     myInfoPage.savePersonalDetails();
   });
